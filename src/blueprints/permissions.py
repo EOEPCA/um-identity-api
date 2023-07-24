ef5e32762034085fa98ca2dca188d0fc71742696
flaviorosadme <flavio.rosa@deimos.com.pt>
2023-07-24 17:15:30 +0100

    feat: last resource permissions endpoints added and working

--- src/blueprints/permissions.py
+++ src/blueprints/permissions.py
@@ -13,32 +13,37 @@ def construct_blueprint(keycloak_client):
     def get_client_management_permissions(client_id: str):
         return keycloak_client.get_client_management_permissions(client_id)
     
-    @permissions.route("/client_authz_scope_permissions/<client_id>/<scope_id>", methods=["GET"])
-    def get_client_authz_scope_permissions(client_id: str, scope_id: str):
-        return keycloak_client.get_client_authz_scope_permissions(client_id, scope_id)
+    @permissions.route("/client_authz_resource_permissions/<client_id>", methods=["GET"])
+    def get_client_resource_permissions(client_id: str):
+        return keycloak_client.get_client_resource_permissions(client_id)
     
-    @permissions.route("/client_authz_scope_permissions/<client_id>", methods=["POST"])
-    def create_client_authz_scope_based_permissions(client_id: str):
-        payload = request.get_json()
-        return keycloak_client.create_client_authz_scope_based_permission(client_id, payload)
+    #@permissions.route("/client_authz_scope_permissions/<client_id>/<scope_id>", methods=["GET"])
+    #def get_client_authz_scope_permissions(client_id: str, scope_id: str):
+    #    return keycloak_client.get_client_authz_scope_permissions(client_id, scope_id)
+    
+    #@permissions.route("/client_authz_scope_permissions/<client_id>", methods=["POST"])
+    #def create_client_authz_scope_based_permissions(client_id: str):
+    #    payload = request.get_json()
+    #    return keycloak_client.create_client_authz_scope_based_permission(client_id, payload)
     
     @permissions.route("/client_authz_resource_permissions/<client_id>", methods=["POST"])
     def create_client_authz_resource_based_permission(client_id: str):
         payload = request.get_json()
-        return keycloak_client.create_client_authz_scope_based_permission(client_id, payload)
+        return keycloak_client.create_client_authz_resource_based_permission(client_id, payload)
 
     @permissions.route("/client_management_permissions/<client_id>", methods=["PUT"])
     def update_client_management_permissions(client_id: str):
         payload = request.get_json()
         return keycloak_client.update_client_management_permissions(client_id, payload)
+    
+    @permissions.route("/client_authz_resource_permissions/<client_id>/<permission_id>", methods=["PUT"])
+    def update_client_authz_resource_permission(client_id: str, permission_id):
+        payload = request.get_json()
+        return keycloak_client.update_client_authz_resource_permission(client_id, payload, permission_id)
 
     @permissions.route("/client_authz_scope_permissions/<client_id>/<scope_id>", methods=["PUT"])
     def update_client_authz_scope_permissions(client_id: str, scope_id):
         payload = request.get_json()
         return keycloak_client.update_client_authz_scope_permission(client_id,  payload, scope_id)
-    
-    @permissions.route("/scopes", methods=["GET"])
-    def get_client_scopes():
-        return keycloak_client.get_client_scopes()
 
     return permissions
