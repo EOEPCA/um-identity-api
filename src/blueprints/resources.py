c170db2c8d41f0425124a338a45d78b00a089926
flaviorosadme <82375986+flaviorosadme@users.noreply.github.com>
2023-10-09 11:32:33 +0100

    Api testing (#18)
    * feat: added client_id as param to enpoints and other fixes
    
    * added changes for permissions endpoints

--- src/blueprints/resources.py
+++ src/blueprints/resources.py
@@ -5,34 +5,26 @@ def construct_blueprint(keycloak_client):
     keycloak_client = keycloak_client
     resources = Blueprint('resources', __name__)
 
-    @resources.route("/resources", methods=["GET"])
-    def get_resources():
-        name = request.args.get('name', "")
-        exact_name = request.args.get('exact_name', False)
-        uri = request.args.get('uri', "")
-        owner = request.args.get('owner', "")
-        resource_type = request.args.get('resource_type', "")
-        scope = request.args.get('scope', "")
-        first = int(request.args.get('first', 0))
-        maximum = int(request.args.get('maximum', -1))
-        return keycloak_client.get_resources(name, exact_name, uri, owner, resource_type, scope, first, maximum)
+    @resources.route("/<client_id>/resources", methods=["GET"])
+    def get_resources(client_id: str):
+        return keycloak_client.get_resources(client_id)
 
     @resources.route("/resources/<resource_id>", methods=["GET"])
     def get_resource(resource_id: str):
         return keycloak_client.get_resource(resource_id)
 
-    @resources.route("/resources", methods=["POST"])
-    def register_resource():
+    @resources.route("/<client_id>/resources", methods=["POST"])
+    def register_resource(client_id: str ):
         resource = request.get_json()
-        return keycloak_client.register_resource(resource)
+        return keycloak_client.register_resource(resource, client_id)
 
-    @resources.route("/resources/<resource_id>", methods=["PUT"])
-    def update_resource(resource_id: str):
+    @resources.route("/<client_id>/resources/<resource_id>", methods=["PUT"])
+    def update_resource(client_id: str, resource_id: str):
         resource = request.get_json()
-        return keycloak_client.update_resource(resource_id, resource)
+        return keycloak_client.update_resource(resource_id, resource, client_id)
 
-    @resources.route("/resources/<resource_id>", methods=["DELETE"])
-    def delete_resource(resource_id: str):
-        return keycloak_client.delete_resource(resource_id)
+    @resources.route("/<client_id>/resources/<resource_id>", methods=["DELETE"])
+    def delete_resource(client_id: str, resource_id: str):
+        return keycloak_client.delete_resource(resource_id, client_id)
 
     return resources
