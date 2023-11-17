5b768a5c35dc1b5563c9f95d498418e04339adf2
Daniel Pimenta <105205108+daniel-pimenta-DME@users.noreply.github.com>
2023-11-17 14:13:51 +0000

    Release (#31)
    * Fix develop dockerfile
    
    * Change keycloak urls
    
    * Fix develop workflow tag
    
    * Fix production workflow
    
    * Change log message
    
    * Change config
    
    * Add health check
    
    * Fix health check
    
    * Add ready health endpoint
    
    * Fix issue
    
    * Change workflow filenames
    
    * Eoepca 910 um keycloak develop an identity api based on keycloak api (#17)
    
    * feat: policies endpoints added, not completely
    
    * feat: working on update policies
    
    * feat: all remaining added, still policy update not working, create and update scope based permission not working
    
    * feat: last resource permissions endpoints added and working
    
    * fix: changed pyyaml version from 5.4.1 to 5.3.1
    
    * feat: endpoints changed
    
    * Update README
    
    * Update config
    
    * Update config
    
    * Update config
    
    * Api testing (#18)
    
    * feat: added client_id as param to enpoints and other fixes
    
    * added changes for permissions endpoints
    
    * Update ci
    
    * Update ci
    
    * Release v1.0.0
    
    * Fix ci
    
    * Fix requirements
    
    * Fix ci
    
    * Upgrade flask version
    
    * Update requirements
    
    * feat: added error handling (#23)
    
    * feat: added validator of register and protect resource enpoint to test
    
    * feat: register and protect resources endpoint working
    
    * feat: added delete resources, policies and permissions
    
    * Update ci
    
    * Update ci
    
    * Fix ci
    
    * Add options method to endpoints
    
    * feat: added endpoint to create client, add resources and protect them if provided
    
    * Revert "Add options method to endpoints"
    
    This reverts commit 9d8c034a088b92e325f2cfa0b23cc6e754948767.
    
    * fea: commit fixes
    
    * feat: more fixes, some endpoint were dounbled
    
    * fix: last fix
    
    * Update ci
    
    * fix: policies fix, response now return client id and resources created
    
    * feat: create client default to confidential and authorization enabled
    
    * Convert to FastAPI
    
    * Convert to FastAPI
    
    * changes to models
    
    * Remove file
    
    * Add error handling, pydantic models, files restructuring
    
    * Fix issues
    
    * Handle keycloak error message
    
    * added fildes to models and descriptions
    
    * Add authenticated field
    
    * Clean and reformat
    
    * Point to keycloak client 1.0.0
    
    * Change logging
    
    * Fix readme
    
    * Clean
    
    * Change logging
    
    * Clean
    
    * merge to develop
    
    * added default resource to response list
    
    * Create default resource
    
    * Fix policies issue
    
    * Improvements
    
    * Change keycloak client to v1.0.0
    
    * Clarify readme
    
    * Add log file
    
    * Fix gitignore
    
    * Fix dockerfile
    
    * Change logging
    
    ---------
    
    Co-authored-by: flaviorosadme <82375986+flaviorosadme@users.noreply.github.com>
    Co-authored-by: flaviorosadme <flavio.rosa@deimos.com.pt>

--- app/log.py
+++ app/log.py
@@ -1,7 +1,42 @@
 import logging
 
+from logging.config import dictConfig
+from pydantic import BaseModel
+
 from app.configuration import config
 
+
+
+class LogConfig(BaseModel):
+    """Logging configuration to be set for the server"""
+
+    LOGGER_NAME: str = "mycoolapp"
+    LOG_FORMAT: str = "%(levelprefix)s | %(asctime)s | %(message)s"
+    level = config.get("App", "logging_level")
+    if not level:
+        level = 'INFO'
+    LOG_LEVEL: str = level.upper()
+
+    # Logging config
+    version = 1
+    disable_existing_loggers = False
+    formatters = {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": LOG_FORMAT,
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    }
+    handlers = {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+    }
+    loggers = {
+        LOGGER_NAME: {"handlers": ["default"], "level": LOG_LEVEL},
+    }
 
 def get_logging_level():
     level = config.get("App", "logging_level")
@@ -22,9 +57,5 @@ def get_logging_level():
         return logging.NOTSET
 
 
-logger = logging.getLogger('um-identity-api')
-logging_level = get_logging_level()
-logger.setLevel(logging_level)
-fh = logging.FileHandler('um-identity-api.log')
-fh.setLevel(logging_level)
-logger.addHandler(fh)
+dictConfig(LogConfig().model_dump())
+logger = logging.getLogger("um-identity-api")
