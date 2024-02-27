c36bb59bcd07012df2d7b15a915be30e802cd099
Daniel Pimenta <daniel.pimenta@deimos.com.pt>
2024-02-27 10:01:12 +0000

    Fix public clients creation

--- app/routers/clients.py
+++ app/routers/clients.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.keycloak_client import keycloak
 from app.models.clients import Client
 from app.routers.clients_resources import register_resources
+from app.log import logger
 
 router = APIRouter(
     prefix="/clients",
@@ -15,6 +16,10 @@ def create_client(client: Client):
     resources = client.resources
     client_dict = client.model_dump()
     del client_dict['resources']
+    if 'publicClient' in client_dict and client_dict['publicClient']:
+        # public clients requires authorizationServicesEnabled = false
+        client_dict['authorizationServicesEnabled'] = False
+    logger.info("client_dict: " + str(client_dict))
     response_client = keycloak.create_client(client_dict)
     response = {
         "client": response_client
